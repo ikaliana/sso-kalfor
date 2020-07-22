f660f7d9e42abad9bbcec82c90ba903e7a5b1fae
Indra <ikaliana@yahoo.com>
2020-07-22 08:31:05 +0700

    view untuk Application Management

---
+++ resources/views/applications.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-2">            
+            <passport-applications></passport-applications>
+        </div>
+    </div>
+</div>
+@endsection
