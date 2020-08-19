4465b6b075d4bc5c321fd4d7a480e7d816555828
Indra <ikaliana@yahoo.com>
2020-08-19 17:32:04 +0700

    Update UserSeeder.php
    Update: create seed with Kalfor ready sample users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,12 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Administrator',
-            'email' => 'admin@mail.com',
-            'password' => Hash::make('password'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        DB::table('users')->insert(
+            [ 'name' => 'Administrator', 'email' => 'admin@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'User KLHK 1', 'email' => 'klhk1@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'User KLHK 2', 'email' => 'klhk2@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'Registrant 1', 'email' => 'registrant1@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'Registrant 2', 'email' => 'registrant2@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'Guest User 1', 'email' => 'guest01@mail.com', 'password' => Hash::make('password') ],
+            [ 'name' => 'Guest User 2', 'email' => 'guest02@mail.com', 'password' => Hash::make('password') ],
+        );
     }
 }
