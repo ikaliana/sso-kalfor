377926d1f23d2c8d4f40fe4190ea2a4e25ce22f5
Indra <ikaliana@yahoo.com>
2020-09-10 16:58:02 +0700

    Update SsoServiceController.php
    Change on getUserInfo function -> display user, roles and its access mode to all menus

--- app/Http/Controllers/SsoServiceController.php
+++ app/Http/Controllers/SsoServiceController.php
@@ -21,23 +21,67 @@ class SsoServiceController extends Controller
         if (Auth::check()) {
             $user = $request->user();
             $user->makeHidden('email_verified_at','created_at','updated_at');
+            $copy_user = clone $user;
 
-            //$roles = $user->roles;
-            //foreach ($user->roles as $role) $role->load('modules');
-            //$user->load('roles.modules.application');
+            $apps = [];
+            $roles = collect();
+
+            foreach ($user->roles as $role) {
+                
+                $copy_role = clone $role;
+
+                foreach($role->modules as $menu) {
+                    $menu->load('application:id,name');
+
+                    $app = $menu->application->only(['id','name']);
+                    $app_exists = array_key_exists($app['id'],$apps);
+                    
+                    if($app_exists) $app = $apps[$app['id']];
+                    else {
+                        $app['menus'] = collect();
+                        $apps[$app['id']] = $app;
+                    }
+
+                    $new_menu['id'] = $menu->id;
+                    $new_menu['name'] = $menu->name;
+                    $new_menu['address'] = $menu->address;
+                    $new_menu['access_mode'] = $menu->access_mode->only(['create','read','update','delete']);
+
+                    $app['menus']->add($new_menu);
+
+                }
+
+                //$copy_role['applications'] = $apps;
+                $copy_role['applications'] = collect();
+                foreach($apps as $app) $copy_role['applications']->add($app);
+
+                $roles->add($copy_role);
+            }
+
+            $copy_user['roles'] = $roles;
+            return $copy_user;
+
+        }
+		else return response()->json(['error' => 'API: Unauthenticated User'], 401);
+	}
+
+    public function GetUserInfoOld(Request $request)
+    {
+        if (Auth::check()) {
+            $user = $request->user();
+            $roles = $user->roles;
+            $user->makeHidden('email_verified_at','created_at','updated_at');
 
             foreach ($user->roles as $role) {
                 foreach($role->modules as $module) {
                     $module->load('application:id,name');
                     $module->makeHidden('client_id');
                 }
-                // $role->load('modules');
             }
 
-            return $user;
+            return $user;        
         }
-		else return response()->json(['error' => 'API: Unauthenticated User'], 401);
-	}
+    }
 
     //Hanya digunakan via API/POST (lihat di Route API). Fungsi logout
 	public function Logout(Request $request)
