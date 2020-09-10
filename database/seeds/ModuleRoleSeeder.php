1e10114de149d22ef1c7aa02dd42e530ee56b95f
Indra <ikaliana@yahoo.com>
2020-09-10 16:54:30 +0700

    Update ModuleRoleSeeder.php
    Create relation between dummy menus and roles

--- database/seeds/ModuleRoleSeeder.php
+++ database/seeds/ModuleRoleSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ModuleRoleSeeder extends Seeder
 {
@@ -20,20 +22,20 @@ class ModuleRoleSeeder extends Seeder
     	$module4 = DB::table('modules')->where('address', '/dashboard')->value('id');
     	$module5 = DB::table('modules')->where('address', '/survey')->value('id');
 
-        DB::table('module_role')->insert(
+        DB::table('module_role')->insert([
             [ 'module_id' => $module1, 'role_id' => $role_admin_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
             [ 'module_id' => $module2, 'role_id' => $role_admin_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
             [ 'module_id' => $module3, 'role_id' => $role_admin_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
             [ 'module_id' => $module4, 'role_id' => $role_admin_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
             [ 'module_id' => $module5, 'role_id' => $role_admin_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
-        );
+        ]);
 
-        DB::table('module_role')->insert(
+        DB::table('module_role')->insert([
             [ 'module_id' => $module1, 'role_id' => $role_klhk_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
             [ 'module_id' => $module2, 'role_id' => $role_klhk_id, 'create' => 0, 'read' => 1, 'update' => 0, 'delete' => 0 ],
             [ 'module_id' => $module3, 'role_id' => $role_klhk_id, 'create' => 0, 'read' => 1, 'update' => 0, 'delete' => 0 ],
             [ 'module_id' => $module4, 'role_id' => $role_klhk_id, 'create' => 0, 'read' => 1, 'update' => 0, 'delete' => 0 ],
             [ 'module_id' => $module5, 'role_id' => $role_klhk_id, 'create' => 1, 'read' => 1, 'update' => 1, 'delete' => 1 ],
-        );
+        ]);
     }
 }
