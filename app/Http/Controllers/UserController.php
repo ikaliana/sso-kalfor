8c651b8be48c7011ffad7b2b37fdbd05950f2ad9
Indra <ikaliana@yahoo.com>
2020-09-10 16:57:26 +0700

    Update UserController.php
    update addUser function; implement notifiable for reset password; add registerUser and resetPassword function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,20 @@ use App\Rules\MatchOldPassword;
 use App\Providers\RouteServiceProvider;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Notifications\Notifiable;
 
+//use Faker\Generator as Faker;
+use Faker\Factory as Faker;
 
 class UserController extends Controller
 {
+    use Notifiable;
+    protected $email = '';
+
     protected function return_error($msg,$http_code)
     {
         response()->json(['error' => $msg], $http_code);
@@ -43,20 +50,48 @@ class UserController extends Controller
 
     public function addUser(request $request)
     {	
-    	$user = new User;
-    	$user->name = $request->name;
-    	$user->email = $request->email;
-    	$user->password = Hash::make($request->password);
-    	$user->save();
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        return $this->newUser($request->name,$request->email,$request->password,$request->role_id);
+    	// $user = new User;
+    	// $user->name = $request->name;
+    	// $user->email = $request->email;
+    	// $user->password = Hash::make($request->password);
+    	// $user->save();
 
-    	//get user id
-    	$roleUser = new RoleUser;
-    	$roleUser->user_id = $user->id;
-    	$roleUser->role_id = $request->role_id;
-    	$roleUser->save();
+    	// //get user id
+    	// $roleUser = new RoleUser;
+    	// $roleUser->user_id = $user->id;
+    	// $roleUser->role_id = $request->role_id;
+    	// $roleUser->save();
 
-    	//return "User berhasil dibuat";
-        $roles = $user->roles;
+    	// //return "User berhasil dibuat";
+     //    $roles = $user->roles;
+     //    return response()->json($user, 201);
+    }
+
+    private function newUser($name,$email,$password,$role_id)
+    {
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = Hash::make($password);
+        $user->save();
+
+        //get user id
+        $roleUser = new RoleUser();
+        $roleUser->user_id = $user->id;
+        $roleUser->role_id = $role_id;
+        $roleUser->save();
+
+        //return "User berhasil dibuat";
+        $roles = $user->load('roles');
+
+        $user->sendEmailVerificationNotification();
         return response()->json($user, 201);
     }
 
@@ -132,10 +167,52 @@ class UserController extends Controller
 
         User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
 
-        //session('status')
-        session()->put('status', 'You have successfully change your password!');
+        if ($request->wantsJson()) {
+            //revoke token
+            auth()->user()->token()->revoke();
+            //regenerate token, send back to user
+            $token = auth()->user()->createToken('newToken')->accessToken;
 
+            return new JsonResponse(['newtoken' => $token], 200);
+
+            //return new JsonResponse(['message' => 'Password has been changed'], 200);
+        }
+
+        session()->put('status', 'You have successfully change your password!');
         return redirect('/home');
+    }
+
+    public function registerUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        return $this->newUser($request->name,$request->email,$request->password,3);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([ 'email' => 'required|string|email' ]);
+        $this->email = $request->email;
+
+        if ($user = User::where('email', $this->email)->first())
+        {
+            $faker = Faker::create();
+            $new_password = $faker->password;
+
+            $user->password = Hash::make($new_password);
+            $user->save();
+
+            $this->notify(new \App\Notifications\MailResetPasswordNotification($new_password));
+
+            return response()->json([ 'message' => 'Password reset email sent.' ], 200);
+        } else {
+            return $this->return_error("User tidak ditemukan",400);
+        }
+
     }
 
 }
