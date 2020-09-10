abf75bb59a84af44e3d5a6935b28abbd766d32c0
Indra <ikaliana@yahoo.com>
2020-09-10 16:51:22 +0700

    Update routes\web.php
    Only verified user can login and access applications route

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,19 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
+    session()->forget('status');
     return redirect( Auth::check() ? 'login' : 'home' );
     //return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logoutsso', 'SsoServiceController@Logout')->name('logoutsso');
 Route::middleware('auth')->get('/settings', 'SsoServiceController@Settings')->name('settings');
 Route::middleware('auth')->get('/password/change', 'UserController@showChangePasswordForm')->name('changepasswordget');
 Route::middleware('auth')->post('/password/change', 'UserController@submitChangePassword')->name('changepasswordpost');
+
 Route::middleware('auth')->get('/applications', function () {
     return view('applications');
-})->name('applications');
+})->name('applications')->middleware('verified');
