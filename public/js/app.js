b29828cc06fd6fdb165535a550afa7e769db42f4
Indra <ikaliana@yahoo.com>
2020-10-21 18:52:45 +0700

    Update app.js
    update

--- public/js/app.js
+++ public/js/app.js
@@ -54278,8 +54278,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Indra\sourcecode\laravel\HomesteadCode\passport\server\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Indra\sourcecode\laravel\HomesteadCode\passport\server\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/passport/server/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/passport/server/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
